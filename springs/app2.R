40965349059f594890fd50f2197c2e9582b4fa79
Roger Day <notsofeo@gmail.com>
2016-06-07 14:21:06 -0400

    Button makes spring grow. Careful with the deleteFile option!

--- springs/app2.R
+++ springs/app2.R
@@ -20,25 +20,28 @@ ui <- shinyUI(fluidPage(
 server <- shinyServer(function(input, output, session) {
    rv = reactiveValues(width=400, height=400)
    output$dimensions <- renderText({
-     paste(width=rv$width, rv$width, "  height=", rv$height)
+     paste("width=", rv$width, "  height=", rv$height)
    })
-   output$spring <- renderImage({
+   output$spring <- renderImage(
+     deleteFile=FALSE,
+     {
      #width  <- session$clientData$output_image1_width
      #height <- session$clientData$output_image1_height
      width = rv$width
      height = rv$height
-     
+     #width=50
+     #height=400
      # Return a list containing information about the image
-     list(src = "spring-photo.jpeg",
+     list(src = "spring-photo.gif",
           # contentType = "image/gif",
-          # width = width,
-          # height = height,  
+          width = width,
+          height = height,  
           alt = "This is alternate text")
    })
    observe ({
     if(!is.null(input$start))
       if(input$start > 0) {
-        rv$width = rv$width * 1.2
+        isolate({rv$width = rv$width * 1.2})
       }
    })
 })
