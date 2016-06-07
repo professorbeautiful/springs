fa46ffaa20c07d9d1a017037bdd390470805cf53
Roger Day <notsofeo@gmail.com>
2016-06-07 18:04:33 -0400

    Working well now, with good settings. Does a stutter every couple of seconds. Could be smoother. Will need friction.

--- springs/app2.R
+++ springs/app2.R
@@ -1,28 +1,34 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- shinyUI(fluidPage(
-  actionButton("start", "start"),
-  textOutput("dimensions"),
-   imageOutput("spring")
+ui <- shinyUI(pageWithSidebar(
+  headerPanel("Springs"),
+  sidebarPanel(
+    actionButton("start", "start"),
+    textInput("K1", "K1", value = "4e-2" ),
+    textInput("initPos", "initial displacement", 
+              value = "500" ),
+    textInput("initMom", "initial momentum", 
+              value = "0" ),
+    textInput("gravity", "gravity", 
+              value = "1" )
+    ,
+    textOutput("dimensions")
+  ),
+  mainPanel( imageOutput("spring"))
 ))
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output, session) {
-   rv = reactiveValues(width=40, height=400, 
-                       initialHeight=400, time=0)
-   autoInvalidate <- reactiveTimer(200)
+   rv = reactiveValues(width=40, 
+                       height=0, 
+                       momentum=0, 
+                       slackHeight=300,
+                       time=0)
+   delta_T = 1
+   autoInvalidate <- reactiveTimer(delta_T)
    output$dimensions <- renderText({
-     paste("width=", rv$width, "  height=", rv$height)
+     paste("width=", round(rv$width),
+           "  height=", round(rv$height))
    })
    output$spring <- renderImage(
      deleteFile=FALSE,
@@ -41,15 +47,24 @@ server <- shinyServer(function(input, output, session) {
           alt = "This is alternate text")
    })
    observe ({
+     period = 20
      autoInvalidate()
     if(!is.null(input$start))
       if(input$start > 0) {
         isolate({
-          period = 20
+          if(rv$time == 0) {
+            rv$height = as.numeric(input$initPos)
+            rv$momentum = as.numeric(input$initMom)
+          }
+          else {
+            rv$height = rv$height + rv$momentum * delta_T
+            K1 = as.numeric(input$K1)
+            gravity = as.numeric(input$gravity)
+            force = K1 * (rv$height - rv$slackHeight) +
+                gravity
+            rv$momentum = rv$momentum - force * delta_T
+          }
           rv$time = rv$time+1
-          # for(time in 1:100) {
-            rv$height = rv$initialHeight * 
-              (1 + sin(rv$time/period * 2 * pi))
         })
       }
    })
